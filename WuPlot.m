0148cb1e62604050db625131334f1c227f86fa7f
Ake1999 <34600945+Ake1999@users.noreply.github.com>
2020-04-15 13:08:45 +0100

    Create WuPlot.m

---
+++ WuPlot.m
@@ -0,0 +1,13 @@
+function WuPlot(ta12)
+    Yt=43.8e6; Yc=43.8e6;
+    Y=-Yc+3e5:1e6:Yt-3e5;
+    Xp=zeros(1,size(Y,2));
+    Xn=zeros(1,size(Y,2));
+    for i=1:size(Y,2)
+        [Xp0,Xn0]=Tsai_Wu(Y(i),ta12);
+        Xp(i)=Xp0; Xn(i)=-Xn0;
+    end
+    X=[Xp,flip(Xn),Xp(1)];
+    Y=[Y,flip(Y),Y(1)];
+    plot(X,Y);
+end
